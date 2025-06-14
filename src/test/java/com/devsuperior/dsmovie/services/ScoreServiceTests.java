5dd4994a4c7b6a14f7deff45ade9950fda44b8a8
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-06-14 19:31:31 -0300

    completed ScoreService class tests

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,21 +1,83 @@
 package com.devsuperior.dsmovie.services;
 
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.MovieEntity;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
+import com.devsuperior.dsmovie.tests.UserFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
-	
+
 	@InjectMocks
-	private ScoreService service;
-	
+	private ScoreService scoreService;
+
+	@Mock
+	private UserService userService;
+
+	@Mock
+	private ScoreRepository scoreRepository;
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	private UserEntity userEntity;
+	private ScoreDTO scoreDTO;
+	private ScoreEntity scoreEntity;
+	private MovieEntity movieEntity;
+	private Long movieId, nonExistsMovieId;
+
+	@BeforeEach
+	void setUp() {
+		movieId = 1L;
+		nonExistsMovieId = 2L;
+		
+		scoreEntity = ScoreFactory.createScoreEntity();
+		userEntity = UserFactory.createUserEntity();
+		movieEntity = MovieFactory.createMovieEntity();
+		scoreDTO = ScoreFactory.createScoreDTO();
+
+		Mockito.when(userService.authenticated()).thenReturn(userEntity);
+		Mockito.when(movieRepository.findById(movieId)).thenReturn(Optional.of(movieEntity));
+		Mockito.when(scoreRepository.saveAndFlush(any())).thenReturn(scoreEntity);
+		Mockito.when(movieRepository.save(any())).thenReturn(movieEntity);
+
+		Mockito.when(movieRepository.findById(nonExistsMovieId)).thenThrow(ResourceNotFoundException.class);
+	}
+
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
+
+		MovieDTO result = scoreService.saveScore(scoreDTO);
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(movieId, result.getId());
 	}
-	
+
 	@Test
 	public void saveScoreShouldThrowResourceNotFoundExceptionWhenNonExistingMovieId() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			movieRepository.findById(nonExistsMovieId);
+		});
 	}
 }
